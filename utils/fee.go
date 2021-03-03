173f33b40655b82ea992171c6352c9a13af2308c
Cuong Tran <cuong@incognito.org>
2021-03-03 17:24:38 +0700

    change to fee per unshield request

--- utils/fee.go
+++ utils/fee.go
@@ -12,7 +12,8 @@ type FeeAPIResponseBody struct {
 	HourFee     uint64 `json:"hourFee"`
 }
 
-func GetNewFee(txSize int, oldFee uint64) (uint64, error) {
+func GetNewFee(txSize int, oldFeePerUnshieldRequest uint, numberOfUnshieldRequest uint) (uint, error) {
+	oldFee := uint64(oldFeePerUnshieldRequest) * uint64(numberOfUnshieldRequest)
 	response, err := http.Get("https://bitcoinfees.earn.com/api/v1/fees/recommended")
 	if err != nil {
 		return 0, err
@@ -33,7 +34,10 @@ func GetNewFee(txSize int, oldFee uint64) (uint64, error) {
 		newFee = responseBody.FastestFee * uint64(txSize)
 	}
 	if newFee < oldFee {
-		newFee = uint64(float64(oldFee) * 1.1)
+		newFee = uint64(float64(oldFee) * 1.2)
 	}
-	return newFee, nil
+
+	newFeePerUnshieldRequest := uint((newFee-1)/uint64(numberOfUnshieldRequest) + 1)
+
+	return newFeePerUnshieldRequest, nil
 }
