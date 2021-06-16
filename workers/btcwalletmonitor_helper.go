e9934c24675b3dea4e1de5cbc3fdb4460d93f2d0
Cuong Tran <cuong@incognito.org>
2021-06-16 16:07:20 +0700

    Add get tracking list from API

--- workers/btcwalletmonitor_helper.go
+++ workers/btcwalletmonitor_helper.go
@@ -3,6 +3,8 @@ package workers
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
 
@@ -10,6 +12,17 @@ import (
 	"github.com/inc-backend/go-incognito/publish/transformer"
 	"github.com/incognitochain/portal-workers/entities"
 )
+
+type PortalAddressInstance struct {
+	IncAddress string `json:"incaddress"`
+	BTCAddress string `json:"btcaddress"`
+	TimeStamp  int64  `json:"timestamp"`
+}
+
+type PortalBackendRes struct {
+	Result []*PortalAddressInstance
+	Error  interface{}
+}
 
 func (b *BTCWalletMonitor) submitShieldingRequest(incAddress string, proof string) (string, error) {
 	metadata := map[string]interface{}{
@@ -73,8 +86,36 @@ func (b *BTCWalletMonitor) getRequestShieldingStatus(txID string) (int, string, 
 }
 
 func (b *BTCWalletMonitor) getTrackingInstance(from int64, to int64) ([]*ShieldingMonitoringInfo, error) {
-	// TODO
-	return []*ShieldingMonitoringInfo{}, nil
+	list := []*ShieldingMonitoringInfo{}
+
+	apiURL := fmt.Sprintf("%v/getlistportalshieldingaddress?from=%v&to=%v", os.Getenv("BACKEND_API_HOST"), from, to)
+	response, err := http.Get(apiURL)
+	if err != nil {
+		return list, err
+	}
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return list, err
+	}
+	var responseBody PortalBackendRes
+	err = json.Unmarshal(responseData, &responseBody)
+	if err != nil {
+		return list, err
+	}
+	if responseBody.Error != nil {
+		return list, fmt.Errorf(responseBody.Error.(string))
+	}
+
+	for _, instance := range responseBody.Result {
+		list = append(list, &ShieldingMonitoringInfo{
+			IncAddress:  instance.IncAddress,
+			BTCAddress:  instance.BTCAddress,
+			TimeStamp:   instance.TimeStamp,
+			ScannedTxID: map[string]int64{},
+		})
+	}
+
+	return list, nil
 }
 
 // hashProof returns the hash of shielding proof (include tx proof and user inc address)
