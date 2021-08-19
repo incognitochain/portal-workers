5434e67f5e7aeae486e17f6540aba9b3c8076060
Cuong Tran <cuong@incognito.org>
2021-08-19 20:27:39 +0700

    Reduce requests on getting external fee

--- utils/fee.go
+++ utils/fee.go
@@ -1,41 +1,9 @@
 package utils
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-)
-
 const (
 	OVERPAYING_RATE              = 1.1
 	DEFAULT_RATE_INCREASING_STEP = 1.15
 )
-
-type BlockCypherFeeResponse struct {
-	HighFee   uint `json:"high_fee_per_kb"`
-	MediumFee uint `json:"medium_fee_per_kb"`
-	LowFee    uint `json:"low_fee_per_kb"`
-}
-
-// Get fee in the current bitcoin condition (satoshi / vbyte)
-func GetCurrentRelayingFee() (uint, error) {
-	response, err := http.Get("https://api.blockcypher.com/v1/btc/main")
-	if err != nil {
-		return 0, err
-	}
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return 0, err
-	}
-	var responseBody BlockCypherFeeResponse
-	err = json.Unmarshal(responseData, &responseBody)
-	if err != nil {
-		return 0, err
-	}
-	feePerVByte := float64(responseBody.MediumFee) / 1024
-
-	return uint(feePerVByte), nil
-}
 
 // Get new fee per request (nano pToken)
 func GetNewFee(vsize int, oldFeePerUnshieldRequest uint, numberOfUnshieldRequest uint, currentRelayingFee uint) uint { // pToken
---
+++ workers/utils.go
@@ -0,0 +1,51 @@
+package workers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var feePerVByte float64 // satoshi / byte
+var feeRWLock sync.RWMutex
+
+type BlockCypherFeeResponse struct {
+	HighFee   uint `json:"high_fee_per_kb"`
+	MediumFee uint `json:"medium_fee_per_kb"`
+	LowFee    uint `json:"low_fee_per_kb"`
+}
+
+func getCurrentRelayingFee() {
+	for {
+		func() {
+			response, err := http.Get("https://api.blockcypher.com/v1/btc/main")
+			feeRWLock.Lock()
+			defer func() {
+				feeRWLock.Unlock()
+				time.Sleep(1 * time.Minute)
+			}()
+			if err != nil {
+				feePerVByte = -1
+				return
+			}
+			if response.StatusCode != 200 {
+				feePerVByte = -1
+				return
+			}
+			responseData, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				feePerVByte = -1
+				return
+			}
+			var responseBody BlockCypherFeeResponse
+			err = json.Unmarshal(responseData, &responseBody)
+			if err != nil {
+				feePerVByte = -1
+				return
+			}
+			feePerVByte = float64(responseBody.MediumFee) / 1024
+		}()
+	}
+}
