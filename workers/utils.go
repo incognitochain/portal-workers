922285f59c5a35dd87f67bcf56617d71909c0f30
Cuong Tran <cuong@incognito.org>
2021-10-03 08:06:09 +0700

    Hard code bitcoin fee

--- workers/utils.go
+++ workers/utils.go
@@ -1,9 +1,6 @@
 package workers
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
 	"sync"
 	"time"
 )
@@ -20,32 +17,12 @@ type BlockCypherFeeResponse struct {
 func getCurrentRelayingFee() {
 	for {
 		func() {
-			response, err := http.Get("https://api.blockcypher.com/v1/btc/main")
 			feeRWLock.Lock()
 			defer func() {
 				feeRWLock.Unlock()
 				time.Sleep(3 * time.Minute)
 			}()
-			if err != nil {
-				feePerVByte = -1
-				return
-			}
-			if response.StatusCode != 200 {
-				feePerVByte = -1
-				return
-			}
-			responseData, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				feePerVByte = -1
-				return
-			}
-			var responseBody BlockCypherFeeResponse
-			err = json.Unmarshal(responseData, &responseBody)
-			if err != nil {
-				feePerVByte = -1
-				return
-			}
-			feePerVByte = float64(responseBody.MediumFee) / 1024
+			feePerVByte = 6
 		}()
 	}
 }
