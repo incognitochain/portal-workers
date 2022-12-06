a5d02f77f9eeaece47e37eede7e7dadda1ec3fa0
0xkraken <hien.n@autonomous.nyc>
2022-12-06 16:33:49 +0700

    Unit test to get unshield fee

--- workers/utxos_test.go
+++ workers/utxos_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestCallPortalVault(t *testing.T) {
-	beaconHeight := uint64(2268879)
+	beaconHeight := uint64(2551081)
 
 	rpcClient := utils.NewHttpClient("https://mainnet.incognito.org/fullnode", "", "", "")
 	btcID := "b832e5d3b1f01a4f0623f7fe91d6673461e1f5d37d91fe78c5c2e6183ff39696"
@@ -50,4 +50,62 @@ func TestCallPortalVault(t *testing.T) {
 	fmt.Printf("count2: %v\n", count2)
 	fmt.Printf("count3: %v\n", count3)
 	fmt.Printf("count4: %v\n", count4)
+}
+
+type UnshieldInfo struct {
+	ExternalFee        uint64
+	NumUnshieldInBatch int
+	NumUTXOs           int
+}
+
+func TestGetUnshieldFee(t *testing.T) {
+	fromBeaconHeight := uint64(2391000)
+	// toBeaconHeight := uint64(2390720)
+	toBeaconHeight := uint64(2520290)
+
+	rpcClient := utils.NewHttpClient("https://mainnet.incognito.org/fullnode", "", "", "")
+	btcID := "b832e5d3b1f01a4f0623f7fe91d6673461e1f5d37d91fe78c5c2e6183ff39696"
+	// rpcClient := utils.NewHttpClient("https://testnet.incognito.org/fullnode", "", "", "")
+	// btcID := "4584d5e9b2fc0337dfb17f4b5bb025e5b82c38cfa4f54e8a3d4fcdd03954ff82"
+
+	unshieldReqs := map[string]UnshieldInfo{}
+	for blk := fromBeaconHeight; blk <= toBeaconHeight; blk+=30 {
+		portalState, err := GetPortalState(blk, rpcClient)
+		if err != nil {
+			fmt.Printf("Err get portal state: %v\n", err)
+			return
+		}
+
+		// get processed batch
+		batchs := portalState.ProcessedUnshieldRequests[btcID]
+		// if len(batchs) == 0 {
+		// 	fmt.Println("Batch empty!")
+		// }
+		for _, batch := range batchs {
+			unshieldIDs := batch.UnshieldsID
+			numUnshieldInBatch := len(unshieldIDs)
+			numUXTOs := len(batch.Utxos)
+
+			for _, unshieldID := range unshieldIDs {
+				if _, ok := unshieldReqs[unshieldID]; ok {
+					continue
+				}
+
+				// get unshield status
+				unshieldStatus, err := getRequestUnShieldingStatus(rpcClient, unshieldID)
+				if err != nil || unshieldStatus.Status != 2 {
+					fmt.Printf("Err get unshield ID %v: %v\n", unshieldID, err)
+					return
+				}
+
+				unshieldReqs[unshieldID] = UnshieldInfo{
+					ExternalFee:        unshieldStatus.ExternalFee,
+					NumUnshieldInBatch: numUnshieldInBatch,
+					NumUTXOs:           numUXTOs,
+				}
+
+				fmt.Printf("AAA,%v,%v,%v\n", unshieldStatus.ExternalFee, numUnshieldInBatch, numUXTOs)
+			}
+		}
+	}
 }
