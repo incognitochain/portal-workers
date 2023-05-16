4f5c99e93bc666b4210ffbb84d0f147006faaee4
0x2525 <2525@generative.xyz>
2023-05-16 10:35:49 +0700

    update get new fee

--- utils/fee.go
+++ utils/fee.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	DEFAULT_RATE_INCREASING_STEP = 1.23
+	DEFAULT_RATE_INCREASING_STEP = 1.25
 )
 
 // Get new fee per request (nano pToken)
@@ -21,17 +21,18 @@ func GetNewFee(vsize int, oldFeePerUnshieldRequest uint, numberOfUnshieldRequest
 	if newFee <= oldFee || newFee > uint64(float64(oldFee)*DEFAULT_RATE_INCREASING_STEP) {
 		newFee = uint64(float64(oldFee) * DEFAULT_RATE_INCREASING_STEP)
 	}
-	newFeePerUnshieldRequest := uint((newFee-1)/uint64(numberOfUnshieldRequest)+1) * 10 // newFeePerUnshieldRequest: nano pBTC
+	newFeePerUnshieldRequest := uint((newFee-1)/uint64(numberOfUnshieldRequest)+1) * 10 // newFeePerUnshieldRequest: nano pBTCs
 
 	return newFeePerUnshieldRequest
 }
 
 func IsEnoughFee(vsize int, feePerRequest uint, numberOfRequest uint, currentRelayingFee uint) bool {
-	overpayingFeePercentStr := os.Getenv("OVERPAYING_FEE_PERCENT")
-	overpayingFeePercent, _ := strconv.Atoi(overpayingFeePercentStr)
-	overpayingRate := 1 + float64(overpayingFeePercent)/100
+	// overpayingFeePercentStr := os.Getenv("OVERPAYING_FEE_PERCENT")
+	// overpayingFeePercent, _ := strconv.Atoi(overpayingFeePercentStr)
+	// overpayingRate := 1 + float64(overpayingFeePercent)/100
 
 	fee := uint64(feePerRequest) * uint64(numberOfRequest) / 10 // fee: satoshi, feePerRequest: nano pToken
-	expectedFee := uint64(float64(currentRelayingFee) * float64(vsize) * overpayingRate)
+	expectedFee := uint64(float64(currentRelayingFee) * float64(vsize))
+	// expectedFee := uint64(float64(currentRelayingFee) * float64(vsize) * overpayingRate)
 	return fee >= expectedFee
 }
